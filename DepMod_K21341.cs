4da6daddeaff6929c99525e2158a1dae47798514
Kamiyo <kamiyo.k91@gmail.com>
2023-01-11 10:00:49 +0100

    Add project files.

---
+++ DepMod_K21341.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace DepMod
+{
+    [BepInPlugin("Dep.Kamiyo.Mods", "Dep Mod", "1.0")]
+    public class DepMod_K21341 : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new Harmony("Dep.Kamiyo.Mods").CreateClassProcessor(typeof(HarmonyPatch_K21341)).Patch();
+        }
+    }
+}
---
+++ HarmonyPatch_K21341.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace DepMod
+{
+    [HarmonyPatch]
+    public class HarmonyPatch_K21341
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HallWorld), nameof(HallWorld.RoleFinishModuleCheck))]
+        public static void HallWorld_RoleFinishModuleCheck(ref bool __result)
+        {
+            __result = false;
+        }
+    }
+}
